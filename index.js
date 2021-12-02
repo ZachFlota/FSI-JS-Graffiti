3223911643e7717e792a8c20d4efb9c40dd504d4
Zachary Flota <zflota1@gmail.com>
2021-12-01 19:18:12 -0500

    Added border radius

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
+let header = document.querySelector('#page-header')
+header.style.textAlign = "left"
+let dogImages = document.querySelectorAll('.dog-image')
+for(let i = 0; i < dogImages.length; i++){
+    dogImages[i].styleborderRadius = '50px'
+}
+
 
 // Select some elements...
